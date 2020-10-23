25ea1d99ec139dc2bd061f4ecc7ad3a56ab8ecab
ga-ha-clam <68629603+ga-ha-clam@users.noreply.github.com>
2020-10-23 15:09:00 +0900

    問題３終了

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,43 @@
 $(function(){
+    
+    let txt1= "えーーーーーー！？";
+    let txt2= "いいのぉ！？";
+    let txt3= "ありがとぉ〜";
+    let num1= 4826;
+    let num2= 7294;
+
+    function sayHallo(){
+        $("#content").append("いよぉ〜〜ポン！");
+    }
+
+    function saySomething(txt){
+        $("#content").append(txt1);
+    }
+
+    function samtwo(arg1,arg2){
+        $("#content").append(arg1 + arg2);
+    }
+  
 
 
-
-
+    $("#button01").on("click",function(){
+        sayHallo();
+    })
+  
+    $("#button02").on("click",function(){
+        saySomething();
+    })
+      
+    $("#button03").on("click",function(){
+        samtwo(txt1,txt2)
+    })
+      
+    $("#button04").on("click",function(){
+        samtwo(num1,num2)
+    })
+  
+    $("#button05").on("click",function(){
+        samtwo(txt1,txt2);
+        samtwo(num1,num2);
+    })
 })
