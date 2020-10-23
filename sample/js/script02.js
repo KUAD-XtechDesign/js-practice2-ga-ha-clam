f8aa399c6967ce13d1f44092f83069abd150b34a
ga-ha-clam <68629603+ga-ha-clam@users.noreply.github.com>
2020-10-23 16:30:04 +0900

    本日のノルマ達成

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -2,12 +2,18 @@ $(function(){
     
     let hako = 0;
 
-    function checkBox();{
-        if(hako > 5){
-          $("#content").text(hako + "個入っています。箱はいっぱいです。空にします");
+    function checkBox(){
+        if(hako > 13){
+          $("#content").append(hako + "個入っています。箱はいっぱいです。空にします");
+          $("#content").addClass('full')
            hako = 0;
+        }else if(hako <= 0){
+          hako = 0;
+          $("#content").append(hako + "個。入っていません。");
+          $("#content").removeClass()
         }else{
-          $("#content").text(hako + "個入っています。まだ入ります");
+          $("#content").append(hako + "個入っています。まだ入ります");
+          $("#content").removeClass()
         }
     }
 
@@ -16,12 +22,12 @@ $(function(){
     })
   
     $("#button02").on("click",function(){
-       hako = hako + 1;
+       hako = hako + 3;
        checkBox()
     })
       
     $("#button03").on("click",function(){
-       hako = hako - 1;
+       hako = hako - 4;
        checkBox()
     })
 
